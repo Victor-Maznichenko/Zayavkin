2257746c3b8683f1fc57cc1dfb8dc465529a065b
Victor-Maznichenko <66438596+Victor-Maznichenko@users.noreply.github.com>
2020-09-29 15:54:58 +0300

    уфкаысч
    куавыяч

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function(){
-	
+
 	$('.menu-btn').on('click', function(){
 		$(this).toggleClass('menu-btn--active');
-		$('.header__menu').toggleClass('header__menu--active');
+		$('.header__menu').slideToggle()
 	});
-	
+
 });
