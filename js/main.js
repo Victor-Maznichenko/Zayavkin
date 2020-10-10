e1624cb1c5d72dd1d97ef31866c47e42ad09f4d6
Victor-Maznichenko <66438596+Victor-Maznichenko@users.noreply.github.com>
2020-10-10 13:54:04 +0300

    rytjghk
    yfguh

--- js/main.js
+++ js/main.js
@@ -5,4 +5,10 @@ $(function(){
 		$('.header__menu').slideToggle()
 	});
 
+	$(".header__menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
 });
